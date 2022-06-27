7a993ba3625f51fb9a6eee7fd7cc9085cd11a000
tomes_f <filip.tomes3@gmail.com>
2022-06-27 20:27:12 +0200

    updated instructors data

--- src/data/instructorsList.js
+++ src/data/instructorsList.js
@@ -1,37 +1,30 @@
-import anet from '../assets/img/instruktori/anet.jpg'
-import david from '../assets/img/instruktori/david.jpg'
-import filip from '../assets/img/instruktori/filip.jpg'
-import kajka from '../assets/img/instruktori/kajka.jpg'
-import martin from '../assets/img/instruktori/martin.jpg'
-import marus from '../assets/img/instruktori/marus.jpg'
-import michelle from '../assets/img/instruktori/michelle.jpg'
-import miso from '../assets/img/instruktori/miso.jpg'
-import nikolas from '../assets/img/instruktori/nikolas.jpg'
-import peta from '../assets/img/instruktori/peta.jpg'
-import petko from '../assets/img/instruktori/petko.jpg'
-import RS from '../assets/img/instruktori/rasto.jpg'
-import simca from '../assets/img/instruktori/simca.jpg'
-import smeky from '../assets/img/instruktori/smeky.jpg'
-import vendy from '../assets/img/instruktori/vendy.jpg'
-import verca from '../assets/img/instruktori/verca.jpg'
+import adela from '../assets/img/instruktori/adela.png'
+import alenka from '../assets/img/instruktori/alenka.png'
+import dave from '../assets/img/instruktori/dave.png'
+import honza from '../assets/img/instruktori/honza.png'
+import iva from '../assets/img/instruktori/iva.png'
+import karel from '../assets/img/instruktori/karel.png'
+import kika from '../assets/img/instruktori/kika.png'
+import klara from '../assets/img/instruktori/klara.png'
+import martas from '../assets/img/instruktori/martas.png'
+import marus from '../assets/img/instruktori/marus.png'
+import sara from '../assets/img/instruktori/sara.png'
+import viv from '../assets/img/instruktori/viv.png'
+
 
 const instructors = [
-  {	name: 'MIŠO', desc: 'Too šrot to handle.', pic: miso },
-  {	name: 'SIMČA', desc: 'Na faktúrku!', pic: simca },
-  {	name: 'RASŤO', desc: 'Keby je Slovensko človek, som jeho skaženejší brat.', pic: RS },
-  {	name: 'MARTIN', desc: 'Improvizace, spontánnost a pomoc druhým jsou pro mě nejdůležitější…', pic: martin },
-  {	name: 'PEŤA', desc: 'Je pro všechno, a pro co není, pro to bude.', pic: peta },
-  {	name: 'MICHELLE', desc: 'Ja vám pustím žilou.', pic: michelle },
-  {	name: 'VENDY', desc: 'Dokud hraje hudba a je co pít, nepřestávám žít.', pic: vendy },
-  {	name: 'PEŤKO', desc: 'Benchuj, dřepuj, tahej a můžeš chodit nahej! ', pic: petko },
-  {	name: 'MARUŠ', desc: 'Jídlo a Malibu, to je něco pro Maju.', pic: marus },
-  {	name: 'DAVID', desc: 'Říká o sobě, že se narodil pérem vzad.', pic: david },
-  {	name: 'VERČA', desc: 'Pokud jsi pes, jsi můj člověk.', pic: verca },
-  {	name: 'SMEKY', desc: 'Gentleman s duší hříšníka a exhibicionisty.', pic: smeky },
-  {	name: 'KAJKA', desc: 'Pokud máš jídlo a vodku, jsme kámoši.', pic: kajka },
-  {	name: 'ANET', desc: 'Ať už jsi kdokoliv, za GIN a TONIC s tebou půjdu kamkoliv.', pic: anet },
-  {	name: 'NIKOLAS', desc: 'Znám rozdíl mezi klitorisem a Gambrinusem 10°.', pic: nikolas },
-  {	name: 'FILIP', desc: 'Ano, chodím pozdě, protože jak se říká, to nejlepší nakonec!', pic: filip },
+  {	name: 'Adéla', desc: 'Too šrot to handle.', pic: adela },
+  {	name: 'Alenka', desc: 'Na faktúrku!', pic: alenka },
+  {	name: 'David', desc: 'Keby je Slovensko človek, som jeho skaženejší brat.', pic: dave },
+  {	name: 'Honza', desc: 'Improvizace, spontánnost a pomoc druhým jsou pro mě nejdůležitější…', pic: honza },
+  {	name: 'Ivča', desc: 'Je pro všechno, a pro co není, pro to bude.', pic: iva },
+  {	name: 'Karel', desc: 'Ja vám pustím žilou.', pic: karel },
+  {	name: 'Kika', desc: 'Dokud hraje hudba a je co pít, nepřestávám žít.', pic: kika },
+  {	name: 'Klára', desc: 'Benchuj, dřepuj, tahej a můžeš chodit nahej! ', pic: klara },
+  {	name: 'Martin', desc: 'Jídlo a Malibu, to je něco pro Maju.', pic: martas },
+  {	name: 'Maruš', desc: 'Říká o sobě, že se narodil pérem vzad.', pic: marus },
+  {	name: 'Sára', desc: 'Pokud jsi pes, jsi můj člověk.', pic: sara },
+  {	name: 'Viv', desc: 'Gentleman s duší hříšníka a exhibicionisty.', pic: viv },
 ]
 
 export default instructors
